7fbd05301d24268853cf0f94dff499edf3a337bf
Thoride <kushdalal007@gmail.com>
2022-08-03 17:32:43 -0400

    Added ability to read from api keys file

--- trader.py
+++ trader.py
@@ -1,8 +1,14 @@
 # Importing the API and instantiating the REST client
 import alpaca_trade_api as api
 
-#region API_KEYS
+#read api keys text file
+with open('api_keys.txt', 'r') as f:
+    api_keys = f.read().splitlines()
 
+#region API_KEYS
+API_KEY = api_keys[0]
+API_SECRET = api_keys[1]
+BASE_URL = "https://paper-api.alpaca.markets"
 #endregion 
 
 alpaca = api.REST(API_KEY, API_SECRET, BASE_URL)
