08638d5e240a287f4d465c2fc3de1c77af8e15c1
Kush <kushdalal007@gmail.com>
2023-01-19 20:12:06 -0500

    Sell strat changes

--- sell_strat_basic.py
+++ sell_strat_basic.py
@@ -13,11 +13,12 @@ BASE_URL = "https://paper-api.alpaca.markets"
 #endregion 
 
 alpaca = api.REST(API_KEY, API_SECRET, BASE_URL)
-account = alpaca.get_account()
+
 
 while True:
+    account = alpaca.get_account()
     percent_change = ((float(account.equity) - float(account.last_equity)) / float(account.last_equity)) * 100
-    print(percent_change)
+    print(str(datetime.now()) + " - " + str(percent_change))
 
     if(percent_change > 0.5):
         #sell all positions
@@ -30,6 +31,17 @@ while True:
             #write to log file
             with open('log.txt', 'a') as f:
                 f.write(str(datetime.now()) + ": " + symbol + ": " + str(qty) + ": " + str(order) + "\n")
-    print("Waiting for 1 hour")
-    time.sleep(3600) #wait 1 hour
+    elif(percent_change < -0.5 and float(account.equity) < 1000):
+        #sell all positions
+        print("SELLING ALL POSITIONS with loss")
+        positions = alpaca.list_positions()
+        for position in positions:
+            symbol = position._raw['symbol']
+            qty = alpaca.get_position(symbol)._raw['qty']
+            order = alpaca.submit_order(symbol, qty=qty, side='sell', type='market', time_in_force='day')
+            #write to log file
+            with open('log.txt', 'a') as f:
+                f.write(str(datetime.now()) + ": " + symbol + ": " + str(qty) + ": " + str(order) + "\n")
+    print("Waiting for 15 Mins")
+    time.sleep(900) #wait 1 hour
     
